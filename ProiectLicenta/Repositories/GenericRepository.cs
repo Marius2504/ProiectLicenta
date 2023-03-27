d502f3d1bf113a26620f50178abbe52f1ad563cd
Marius2504 <marius.dumitrescu@s.unibuc.ro>
2023-03-27 16:34:43 +0300

    GenericRepository

---
+++ ProiectLicenta/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProiectLicenta.Data;
+using ProiectLicenta.Repositories.Interfaces;
+
+namespace ProiectLicenta.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DataContext _context;
+
+        public GenericRepository(DataContext context)
+        {
+            this._context = context;
+        } 
+        public T Add(T entity)
+        {
+           _context.Set<T>().Add(entity);
+           Save();
+           return entity;
+        }
+   
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+        public T Update(T entity)
+        {
+            _context.Entry(entity).State =EntityState.Modified;
+            Save();
+            return entity;
+        }
+        public async void Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+       
+
+    }
+}
---
+++ ProiectLicenta/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ProiectLicenta.Repositories.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        List<T> GetAll();
+        T Add(T entity);
+        T Update(T entity);
+    }
+}
